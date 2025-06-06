5d6974063d957d92f1a0003bd3aebdf42b6e6534
Dhanush Kunchakuri <132380514+dhanushkunchakuri@users.noreply.github.com>
2025-06-06 12:42:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 //your JS code here. If required.
+function display(text,delay){
+
+	return new Promise((resolve)=>{
+		setTimeout(()=>{
+			resolve(text);
+		},delay)
+	})
+}
+ document.getElementById("btn").addEventListener('click',async ()=>{
+	try{
+		let text=document.getElementById("text").value;
+		let delay=parseInt(document.getElementById("delay").value);
+		let	data=await display(text,delay);
+		document.getElementById("output").innerText=data;
+	}
+	 catch(e){
+		 console.log("error:",e);
+	 }
+});
+	
+	
